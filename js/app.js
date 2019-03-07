9e2cb5a1c4af966c2a6fa6ab024ba1562fe7e429
Lyndon Panton <LyndonPanton@gmail.com>
2019-03-07 23:58:16 +0000

    Selected the icon and added a click event listener to it

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 let year = document.getElementById("year");
 let date = new Date();
 year.textContent = `${date.getFullYear()}`;
+
+let achromaticInfo = document.getElementById("did-you-know");
+achromaticInfo.addEventListener("click", function() {
+	console.log("Info icon clicked.");
+});
