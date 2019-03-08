fb2111fb81c9d63e0d450938d60cb6d5c8a83b0b
Lyndon Panton <LyndonPanton@gmail.com>
2019-03-08 22:18:11 +0000

    Gave each element with the class "did-you-know" a click  event listener

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ let year = document.getElementById("year");
 let date = new Date();
 year.textContent = `${date.getFullYear()}`;
 
-let achromaticInfo = document.getElementById("did-you-know");
-achromaticInfo.addEventListener("click", function() {
-	console.log("Info icon clicked.");
+let achromaticInfo = document.getElementsByClassName("did-you-know");
+Array.from(achromaticInfo).forEach(function(icon) {
+	icon.addEventListener("click", function() {
+		console.log("Info icon clicked.");
+	});
 });
